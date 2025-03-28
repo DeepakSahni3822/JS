e9495d681b61963c5a96bcd99e600ecf78466740
DeepakSahni3822 <121127098+DeepakSahni3822@users.noreply.github.com>
2025-03-28 13:19:21 +0000

    variables

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,21 @@
+const accountid = 2217
+let email = "dawson.google.com"
+var city = "New Delhi"
+ // console.log(accountid);
+//console.log(email);
+//console.log(city);
+
+email = "dawson.yahoo.com"
+//console.log(email);
+city = "Delhi"
+//console.log(city);
+
+
+
+
+
+
+
+
+
+
